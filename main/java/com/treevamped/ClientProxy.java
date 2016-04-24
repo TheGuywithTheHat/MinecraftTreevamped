a677ec2cc6f58bc898aa3b83864409fb6ce8bc51
TheGuywithTheHat <ryancarlson486@gmail.com>
2016-04-24 16:18:02 -0400

    Fixed #1
    Particles from the block now appear when sprinting or landing on the
    block.
    The color of the particles is meh, but whatever.

--- main/java/com/treevamped/ClientProxy.java
+++ main/java/com/treevamped/ClientProxy.java
@@ -17,7 +17,12 @@ public class ClientProxy {
     public void init() {
         Minecraft.getMinecraft().getRenderItem().getItemModelMesher()
             .register(TBlock.GROWTH_BLOCK.item, 0, new ModelResourceLocation(TBlock.GROWTH_BLOCK.item.getRegistryName(), "inventory"));
-        Minecraft.getMinecraft().getBlockColors().registerBlockColorHandler((state, access, pos, tintIndex) -> BiomeColorHelper.getFoliageColorAtPos(access, pos), TBlock.GROWTH_BLOCK);
+        Minecraft.getMinecraft().getBlockColors().registerBlockColorHandler((state, access, pos, tintIndex) -> {
+                if(pos != null) {
+                    return BiomeColorHelper.getFoliageColorAtPos(access, pos);
+                }
+                return 0xff00ff00;
+            }, TBlock.GROWTH_BLOCK);
     }
     
     public void postInit() {
